22b204c6372d78658a86f084dd504eaad6370b37
LozzDev <lozzdeveloper@gmail.com>
2024-11-29 09:59:34 +0100

    main changes, minor changes

---
+++ endScript.js
@@ -0,0 +1,5 @@
+import { exportExecutedHumans } from './mainScript.js'; // Importar la función
+
+// Usar la función para obtener el array
+
+console.log(exportExecutedHumans()); // ["Human 1", "Human 2", "Human 3"]
--- mainScript.js
+++ mainScript.js
@@ -82,7 +82,6 @@ const alivehumansCasual = [
     }
 ];
 
-console.log(alivehumansCasual)
 
 //funciones para hallar las coordenadas x e y de los humanos
     
@@ -108,9 +107,7 @@ console.log(alivehumansCasual)
 function humanCoordsCalculator(){
     alivehumansCasual.forEach(human => {
         human.coordsX = humanUbicationX(human);
-        human.coordsY = humanUbicationY(human);
-        console.log(`Coordenada Y de ${human.id}: ${human.coordsY}`);
-        console.log(human)
+        human.coordsY = humanUbicationY(human); 
     })
 }
 
@@ -129,18 +126,15 @@ window.addEventListener("resize", humanCoordsCalculator); //cada vez que se camb
 
 //funcion para trackear en tiempo real la posicion del ojo en todo momento
 
-    let coordsXeye=0;
-    let coordsYeye=0;
-    const doomEye = document.getElementById("eye");
+let coordsXeye=0;
+let coordsYeye=0;
+const doomEye = document.getElementById("eye");
 
 function trackingEyePosition(){
-    
     const coordsEye= doomEye.getBoundingClientRect();
 
     coordsXeye=coordsEye.left; //creo que esto te coge la esquina superior izquierda pero se arregla
     coordsYeye=coordsEye.top;
-
-    //console.log(`Posición X: ${coordsXeye}, Posición Y: ${coordsYeye}`);
     
     requestAnimationFrame(trackingEyePosition);
 }
@@ -149,6 +143,7 @@ trackingEyePosition(); // esto debe estar para que se active la funcion y saber 
 //fpuncion para que el ojo siga al raton en escriotrio
 let mouseX=0;
 let mouseY=0;
+
 function eyeFollowMouse(event){
     mouseX=event.clientX;
     mouseY=event.clientY;
@@ -177,79 +172,48 @@ window.addEventListener("deviceorientation", eyeFollowGyroscope);
 
 function killAliveHumans(aliveHumansArray){
     
-        let randomHuman=Math.floor(Math.random() * aliveHumansArray.length);
+    let randomHuman=Math.floor(Math.random() * aliveHumansArray.length);
         
-        while(aliveHumansArray[randomHuman].alive == false){ //se hace un while por si el numero aleatorio coincide con un muerto y hasta que no de un numero de un vivo no para
-            randomHuman=Math.floor(Math.random() * aliveHumansArray.length);
-        }
+    while(aliveHumansArray[randomHuman].alive == false){ //se hace un while por si el numero aleatorio coincide con un muerto y hasta que no de un numero de un vivo no para
+        randomHuman=Math.floor(Math.random() * aliveHumansArray.length);
+    }
 
-        aliveHumansArray[randomHuman].alive = false;
+    aliveHumansArray[randomHuman].alive = false;
         
     return (aliveHumansArray[randomHuman]);
 }//funciona, esta testeado
 
-
 //funcion que se activa cuando se aprieta el boton
 //cosas que debe hacer --> crear array vivos, elegir 5 humanos a morir, ejecutar animacion de matar
 
- const executeHumansArray=[];
-
+const executeHumansArray=[]; //exportar
 let indexLimitClicker=0;
 let canClick=true;
 
 function executerCasual(){
-//TO-DO
-//cuando se presiona el click se consigue el array de los vivos
-if (!canClick) return; // Si no se puede hacer clic, salimos de la función
+    //TO-DO
+    //cuando se presiona el click se consigue el array de los vivos
+    if (!canClick) return; // Si no se puede hacer clic, salimos de la función
 
     canClick = false; // Bloqueamos nuevos clics
 
     setTimeout(() => {
         canClick = true; // Permitimos clics después de 1 segundo
     }, 900);
-if (indexLimitClicker >= 15) {
+
+    if (indexLimitClicker >= 15) {
     // Eliminar el evento después de 15 clics
+    
     document.removeEventListener("click", executerCasual);
     
     return; // Salimos de la función
-}
+    }
 
 
     const executedHuman = killAliveHumans(alivehumansCasual); //esto es un objeto human
-    executeHumansArray.push(executedHuman);
+    executeHumansArray.push(executedHuman.name);
 
-    //aqui se tiene que hacer para que salga el nombre
-    //se introduce el elemento y sus estilos
     
-        const textElement = document.createElement("div");
-        textElement.textContent = `${executeHumansArray[indexLimitClicker].name} ha sido eliminado`;
-        textElement.style.position = "absolute";
-        textElement.style.bottom = "20px";
-        textElement.style.left = "50%";
-        textElement.style.transform = "translateX(-50%)";
-        textElement.style.color = "white";
-        textElement.style.fontSize = "30px";
-        textElement.style.fontWeight = "bold";
-        textElement.style.opacity = "1";
-        textElement.style.transition = "all 3s ease-out";
-  
-    // Añadir el texto al cuerpo del documento
-    document.body.appendChild(textElement);
-  
-    // Ejecutar la animación después de un pequeño retardo para que de tiempo a que muera el humano
-    setTimeout(() => {
-      textElement.style.transform = "translateX(-50%) translateY(-100px)";
-      textElement.style.opacity = "0";
-    }, 100); 
-  
-    // Eliminar el elemento del DOM después de la animación
-    setTimeout(() => {
-      textElement.remove();
-    }, 3100); // Eliminar después de 3.1 segundos para que de tiempo a la animacion
-  
-
-
-    //----------------------------------------------
 
     //informacion del humano muerto
     let human=document.getElementById(executedHuman.id);
@@ -281,37 +245,65 @@ if (indexLimitClicker >= 15) {
     laser.style.animation = "shootAnimation 0.9s ";
     const doomEyeImage=document.getElementById("eye-image");
 
+    
+    //se introduce el elemento y sus estilos
+    const textElement = document.createElement("div");
+    textElement.textContent = `${executedHuman.name} has been executed`;
+    textElement.style.position = "absolute";
+    textElement.style.bottom = "20px";
+    textElement.style.left = "50%";
+    textElement.style.transform = "translateX(-50%)";
+    textElement.style.color = "white";
+    textElement.style.fontSize = "25px";
+    textElement.style.fontWeight = "bold";
+    textElement.style.opacity = "1";
+    textElement.style.transition = "all 3s ease-out";
+
+    // Añadir el texto al cuerpo del documento
+        
+    document.body.appendChild(textElement);
+        
+    // Ejecutar la animación después de un pequeño retardo para que de tiempo a que muera el humano
+    setTimeout(() => {
+        textElement.style.transform = "translateX(-50%) translateY(-100px)";
+        textElement.style.opacity = "0";
+    }, 100); 
+
+    // Eliminar el elemento del DOM después de la animación
+    setTimeout(() => {
+        textElement.remove();
+    }, 3100); // Eliminar después de 3.1 segundos para que de tiempo a la animacion
+    
+
+
+    //----------------------------------------------
+
     setTimeout(() => {
         laser.style.display = "none"; //ocultar el rayo una vez se lance
-        
         doomEyeImage.setAttribute=("src", "./images/eye-attack.gif"); //TO-DO NO FUNCIONA
         human.style.opacity=0;
-        
     }, 900);
+
     setTimeout(() => {
-            
         human.setAttribute("src", "./images/blood_effect.gif");
         laser.setAttribute("src", "./images/yellowBallExplosion.gif");
     }, 500);
     
     laser.setAttribute("src", "./images/yellowBall.gif");//con estos timeouts timeamos las muertes de los atronautas y generamos su animacion de muerte
     indexLimitClicker++;
+    //aqui se tiene que hacer para que salga el nombre
     
      
     if(indexLimitClicker==15){
         setTimeout(() => {
-            
             window.location.href = "end.html";
         }, 3000 );
         
-    }
+    }  
     
 }
 
 document.addEventListener("click", executerCasual);
-
-
-
 
 
 //efecto onda expansiva cuando dispara
@@ -335,3 +327,89 @@ const createRipple = (e) => {
 }
 
 container.addEventListener('click', createRipple);
+
+//aqui tenemos que borrar todo el html para mostrar el end.html
+
+function createEndPage(arrayExecutedHumans){
+    //con estas 3 lineas borramos el main y activamos el cursor
+    const mainPage=document.getElementById("main");
+    const body = document.body;
+    mainPage.remove();
+    document.body.style.cursor = "default";
+    //-----Aqui comenzamos a crear el end.html aplicando la estructura html de la pagina---------
+
+    // Crear el primer div sin hijos
+    const div1 = document.createElement('div');
+    div1.className = 'parent-div-1';
+    //aqui van los hijos de este div
+
+
+    body.appendChild(div1);
+
+    // Crear el segundo div con 2 hijos
+    const div2 = document.createElement('div');
+    div2.className = 'parent-div-2';
+    //aqui van los hijos de este div
+
+    
+    body.appendChild(div2);
+
+    const div2Child1 = document.createElement('div');
+    div2Child1.className = 'child-div-2-1';
+    //aqui van los hijos de este div
+
+    
+    div2.appendChild(div2Child1);
+
+    const div2Child2 = document.createElement('div');
+    div2Child2.className = 'child-div-2-2';
+    //aqui van los hijos de este div
+
+    
+    div2.appendChild(div2Child2);
+
+    // Crear el tercer div con 3 hijos
+    const div3 = document.createElement('div');
+    div3.className = 'parent-div-3';
+    //aqui van los hijos de este div
+
+    
+    body.appendChild(div3);
+
+    const div3Child1 = document.createElement('div');
+    div3Child1.className = 'child-div-3-1';
+    //aqui van los hijos de este div
+
+    
+    div3.appendChild(div3Child1);
+
+    const div3Child2 = document.createElement('div');
+    div3Child2.className = 'child-div-3-2';
+    //aqui van los hijos de este div
+
+    
+    div3.appendChild(div3Child2);
+
+    const div3Child3 = document.createElement('div');
+    div3Child3.className = 'child-div-3-3';
+    //aqui van los hijos de este div
+
+    
+    div3.appendChild(div3Child3);
+
+    // Crear el cuarto div sin hijos
+    const div4 = document.createElement('div');
+    div4.className = 'parent-div-4';
+    //aqui van los hijos de este div
+
+    
+    body.appendChild(div4);
+
+    //AQUI VA EL FOOTER TO-DO
+    //---------------------
+    //---------------------
+    //---------------------
+    //---------------------
+}
+
+createEndPage(executeHumansArray);
